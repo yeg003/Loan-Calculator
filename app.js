304a05bfd361ec8df36e66777653769fd2ef754e
yeg003 <yeg003@gmail.com>
2019-06-27 23:16:35 -0400

    modified loader to show and dissapear after 2 seconds

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 //Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function (e) {
+    //Hide results
+    document.getElementById('results').style.display = 'none';
+
+    //Show loader
+    document.getElementById('loading').style.display = 'block';
+
+    setTimeout(calculateResults, 2000)
+    e.preventDefault(e)
+});
 
 //Calculate Results function
-function calculateResults(e) {
+function calculateResults() {
     console.log('calculating...')
     //UI variables
     const amount = document.getElementById('amount')
@@ -24,10 +33,17 @@ function calculateResults(e) {
         monthlyPayment.value = monthly.toFixed(2)
         totalPayment.value = (monthly * calculatedPayments).toFixed(2)
         totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2)
+
+        //Show results
+        document.getElementById('results').style.display = 'block';
+
+        //Hide loader
+        document.getElementById('loading').style.display = 'none';
+
     } else {
         showError('Please check your numbers')
     }
-    e.preventDefault()
+
 }
 
 // Show Error function
